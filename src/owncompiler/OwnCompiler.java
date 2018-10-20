404f23654995abb419c7999bb780a2b784a72701
Ashar Ashfaq <Ashar Ashfaq@DESKTOP-VJN62SQ>
2018-10-20 19:01:00 +0500

    finally starting syntax...

--- src/owncompiler/OwnCompiler.java
+++ src/owncompiler/OwnCompiler.java
@@ -5,19 +5,32 @@
  */
 package owncompiler;
 
+
+import owncompiler.lexical.Lexical_analyzer;
+import java.io.IOException;
+import owncompiler.syntax.Syntax;
+
 /**
  *
- * @author Ashar Ashfaq
+ * @author Bismah Rasheed
  */
 public class OwnCompiler {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) throws IOException {
      
         Lexical_analyzer lex = new Lexical_analyzer();
+        Syntax syntax = new Syntax(lex.tokenlist);
+        
+       
+       
+        
         
     }
+    
     
 }
