3520be987468759f43b870462cd9bd7f6c64b23b
Ashar Ashfaq <Ashar Ashfaq@DESKTOP-VJN62SQ>
2018-10-21 01:38:30 +0500

    Function and arguments cfg done.

--- src/owncompiler/syntax/Syntax.java
+++ src/owncompiler/syntax/Syntax.java
@@ -6,32 +6,140 @@
 package owncompiler.syntax;
 import java.util.ArrayList;
 import owncompiler.lexical.Token;
+
 /**
  *
  * @author Ashar Ashfaq
  */
 public class Syntax {
    
+    int index = 0;
     
     
+    CheckKeys checkkey;
     ArrayList<Token> TokenList = new ArrayList<Token>();
     public Syntax(ArrayList<Token> tokenlist){
         this.TokenList = tokenlist;
-        System.out.println("adhasdhadh");
-        System.out.println(""+this.TokenList.get(3).valuePart);
-        
-    }
-    
-    private void AM(){
-        String[] AM = new String[3];
-        AM[0] = "public";
-        AM[1] = "private";
-        AM[2] = "protected";
-        
+        checkkey = new CheckKeys(tokenlist);
         
         
     }
     
     
     
+    private boolean stat(){
+        return ToCheckIfTrue("final") || ToCheckIfTrue("static");
+    }
+    
+    private boolean return_type(){
+            if(checkkey.DT()){
+                return true;
+            }
+            else if(checkkey.ID()){
+                return true;
+            }
+            else if(ToCheckIfTrue("void")){
+                return true;
+            }
+        
+        
+        return false;
+    }
+    private boolean args(){
+        if(ToCheckIfTrue("(")){
+            index++;
+            if(args1()){
+                index++;
+                if(args0()){
+                    index+=2;
+                }
+            }
+        }
+            
+        
+        return false;
+    }
+    
+    private boolean ToCheckIfTrue(String str){
+        return this.TokenList.get(index).classPart.equals(str);
+    }
+    
+    private boolean args1(){
+        return ToCheckIfTrue("DT");
+    }
+    private boolean args0(){
+       if(ToCheckIfTrue("[")){
+           index++;
+           if(ToCheckIfTrue("]")){
+            index++;
+            if(ToCheckIfTrue("ID")){
+                index++;
+                if(args2()){
+                    
+                }
+            }
+           }
+       }
+       else if(ToCheckIfTrue("ID")){
+           index++;
+           if(args2()){
+               return true;
+           }
+       }
+           
+           
+       
+       return false;
+    }
+    private boolean args2(){
+        if(ToCheckIfTrue(","))
+        {
+            index++;
+            if(args1()){
+                index++;
+            }
+        }
+        else{
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean body(){
+        
+        return false;
+    }
+    
+    
+    private boolean func_Dec(){
+         
+            if(checkkey.AM()){
+                index++;
+            }
+            if(stat()){
+                index++;
+            }
+            if(return_type()){
+                index++;
+                if(checkkey.ID()){
+                    index++;
+                        if(args()){
+                            if(body()){
+                                
+                            }
+                        }
+                    
+                }
+            }
+                
+            
+            return false;
+        }
+ 
 }
+
+        
+    
+    
+    
+
