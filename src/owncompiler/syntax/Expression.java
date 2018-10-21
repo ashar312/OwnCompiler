58773ae8e0772cb5f8d5e2bafa271d3203eb5acf
Ashar Ashfaq <Ashar Ashfaq@DESKTOP-VJN62SQ>
2018-10-21 18:32:21 +0500

    Expression almost done.

--- src/owncompiler/syntax/Expression.java
+++ src/owncompiler/syntax/Expression.java
@@ -123,21 +123,196 @@ public class Expression extends Syntax {
         return false;
     }
     private boolean BAE(){
-    
+        if(first()){
+            if(REE()){
+                if(BAEE()){
+                    return true;
+                }
+            }
+        }
         return false;
     }
     private boolean BAEE(){
-    
+        if(ToCheckIfTrue("&")){
+            index++;
+            if(REE()){
+                if(BAEE()){
+                    return true;
+                }
+            }
+        }else if(ToCheckIfTrue("^") ||ToCheckIfTrue("|") || ToCheckIfTrue("&&") 
+                || ToCheckIfTrue("||") || ToCheckIfTrue(";") || ToCheckIfTrue(","))
+        {
+            return true;
+        }
         return false;
     }
     private boolean REE(){
+        if(first()){
+            if(RE1()){
+                if(REEE()){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
     
+    private boolean RE1(){
+        if(first()){
+            if(shift_op()){
+                if(REEE()){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    private boolean RE1E(){
+        if(ToCheckIfTrue("RO")){
+            if(shift_op()){
+                if(REEE()){
+                    return true;
+                }
+            }
+        }
+        else if(ToCheckIfTrue("==") || ToCheckIfTrue("!=") || ToCheckIfTrue("^") 
+                || ToCheckIfTrue("|") || ToCheckIfTrue("&&") || ToCheckIfTrue("||") 
+                || ToCheckIfTrue(";") || ToCheckIfTrue(",")){
+            return true;
+        }
+            
+        return false;
+    }
+    private boolean REEE(){
+        if(ToCheckIfTrue("!=") || ToCheckIfTrue("==")){
+            index++;
+            if(RE1()){
+                if(REEE()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean shift_op(){
+        if(first()){
+            if(E()){
+                if(shift_op1()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean shift_op1(){
+        if(ToCheckIfTrue("ShOP")){
+            index++;
+            if(E()){
+                if(shift_op1()){
+                    return true;
+                }
+            }
+        }
+        else if(ToCheckIfTrue("RO") || signs()){
+            return true;
+        }
+        return false;
+    }
+    private boolean E(){
+        if(first()){
+            if(T()){
+                if(EE()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean EE(){
+        if(ToCheckIfTrue("PM")){
+            index++;
+            if(T()){
+                if(EE()){
+                    return true;
+                }
+            }
+        }
+        else if(ToCheckIfTrue("ShOP") || ToCheckIfTrue("RO") || signs()){
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean T(){
+        if(first()){
+            if(D()){
+                if(TT()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean D(){
+        if(first()){
+            if(F()){
+                if(DD()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean TT(){
+        if(ToCheckIfTrue("MDM")){
+            index++;
+            if(D()){
+                if(TT()){
+                    return true;
+                }
+            }
+        }
+        else if(ToCheckIfTrue("PM") || ToCheckIfTrue("ShOP") || ToCheckIfTrue("RO") || signs()){
+            return true;
+        }
+        return false;
+    }
+    private boolean DD(){
+        if(ToCheckIfTrue("IncDec")){
+            index++;
+        }
+        else if(ToCheckIfTrue("MDM") || ToCheckIfTrue("PM") || ToCheckIfTrue("ShOP") || ToCheckIfTrue("RO") || signs()){
+            
+        }
+        return false;
+    }
+    private boolean F(){
+        if(first()){
+            
+        }
+        return false;
+    }
+    private boolean FF(){
+        
+        return false;
+    }
+    private boolean FFF(){
+        
         return false;
     }
     
     
     
     
-    
+    private boolean signs(){
+       return  ToCheckIfTrue("==") || ToCheckIfTrue("!=") || ToCheckIfTrue("^") 
+                || ToCheckIfTrue("|") || ToCheckIfTrue("&&") || ToCheckIfTrue("||") 
+                || ToCheckIfTrue(";") || ToCheckIfTrue(",");
+    }
     
 }
